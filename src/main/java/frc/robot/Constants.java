23be376a9bc097294f0444e9298fa35f4a473c78
OldManMatt <bigmegamatt@gmail.com>
2023-03-29 21:01:46 -0400

    Motor ports updated

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,9 +3,9 @@ package frc.robot;
 public class Constants {
     
     // declare motor ports
-    public static final byte leftMotor1Port = 0;
-    public static final byte leftMotor2Port = 1;
-    public static final byte rightMotor1Port = 2;
-    public static final byte rightMotor2Port = 3;
+    public static final byte leftMotor1Port = 20;
+    public static final byte leftMotor2Port = 21;
+    public static final byte rightMotor1Port = 10;
+    public static final byte rightMotor2Port = 11;
 
 }
